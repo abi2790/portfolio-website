3fbac42dc85bf54d94e50a22ff92385c402254c1
Abishek K <94035638+abi2790@users.noreply.github.com>
2025-04-15 22:25:22 +0530

    day4

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
+
 import backend from "./backend.png";
+
 import creator from "./creator.png";
+
 import mobile from "./mobile.png";
+
 import web from "./web.png";
+
 import github from "./github.png";
+
 import menu from "./menu.svg";
+
 import close from "./close.svg";
 
 import css from "./tech/css.png";

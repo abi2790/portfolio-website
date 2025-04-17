7376c06f7911ec3da1ffce5ca13bf00001cc6030
Abishek K <94035638+abi2790@users.noreply.github.com>
2025-04-17 17:12:41 +0530

    day6

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,15 @@ import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas'
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
-import Tech from './Tech';
+import { Canvas } from 'react-three-fiber';
+import { OrbitControls } from 'drei';
+import Tech from './Tech';  
 import Experience from './Experience';
+import Earth from './Earth';
+import Ball from './Ball';
 import Works from './Works';
+import Computers from './Computers';
+import Stars from './Stars';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
